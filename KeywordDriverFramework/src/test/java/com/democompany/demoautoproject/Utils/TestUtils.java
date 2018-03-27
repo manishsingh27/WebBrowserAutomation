7487820d25d7fa621194b9ed560cd4eb27ab1216
manis <immanish27@gmail.com>
2018-03-27 10:19:46 -0400

    Intial Commit

---
+++ KeywordDriverFramework/src/test/java/com/democompany/demoautoproject/Utils/TestUtils.java
@@ -0,0 +1,28 @@
+package com.democompany.demoautoproject.Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+
+import com.democompany.demoautoproject.Utils.Resources;
+
+
+public class TestUtils extends Resources{
+
+	public static void getScreenShot(String fileName) throws IOException {
+		File outputFile = driver.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(outputFile,new File(System.getProperty("user.dir")+"//src//test//java//com//companyname//projectname//Screenshot//"+fileName+".jpg"));
+	}
+
+	public static String now(String format) {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat fm = new SimpleDateFormat();
+		return fm.format(cal.getTime());
+	}
+	
+	
+}
